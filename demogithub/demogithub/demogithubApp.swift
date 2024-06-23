1c53f4d1559d257a31143396554e52cc2768733b
admin <admin@admins-MacBook-Pro.local>
2024-06-22 20:24:56 -0700

    init project

---
+++ demogithub/demogithub/demogithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  demogithubApp.swift
+//  demogithub
+//
+//  Created by admin on 22/6/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct demogithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
